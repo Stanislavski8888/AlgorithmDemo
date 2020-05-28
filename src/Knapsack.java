afa0ad81ce66eb331850f2ac7de10d9d08bf9a22
v_huanghao01 <v_huanghao01@baidu.com>
2020-05-28 16:00:46 +0800

    01背包问题

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,21 +1,19 @@
 public class Knapsack {
     private static final int LENGTH = 6;
     private static final int CAPACITY = 20;
-    int[][] memo = new int[LENGTH][];
-
 
     public static void main(String[] args) {
         Knapsack knapsack = new Knapsack();
+        int[][] items = new int[LENGTH][];
         int[] weights = new int[LENGTH];
         int[] values = new int[LENGTH];
+
+        //随机生成weights和values，随后打印
         for (int i = 0; i < LENGTH; i++) {
-            weights[i] = (int)(Math.random() * 10);
-//            System.out.printf("weight[" + i + "] = " + weights[i]);
+            weights[i] = (int)(Math.random() * 10 + 1);
         }
-//        System.out.println();
         for (int i = 0; i < LENGTH; i++) {
             values[i] = (int) (Math.random() * 10 + 1);
-//            System.out.println("value[" + i + "] = " + values[i]);
         }
         StringBuilder wsb = new StringBuilder("weights = [");
         for (int weight : weights) {
@@ -33,28 +31,98 @@ public class Knapsack {
         vsb.append("]");
         System.out.println(vsb.toString());
 
+
         for (int i = 0; i < LENGTH; i++) {
-            knapsack.memo[i] = new int[CAPACITY + 1];
+            items[i] = new int[] {weights[i], values[i]};
         }
-        int best = knapsack.bestValue(weights, values, LENGTH - 1, CAPACITY);
+
+        int best = knapsack.bestValueOpt2(items, CAPACITY);
         System.out.println("best = " + best);
     }
 
-    int bestValue(int[] weight, int[] values, int index, int cap) {
-        if (cap <= 0 || index < 0) {
+    //方法一
+    int bestValue(int[][] items, int cap) {
+        if (cap <= 0 || items.length <= 0) {
             return 0;
         }
-
-        if (memo[index][cap] != -1) {
-            return memo[index][cap];
+        int N = items.length;
+        int[][] memo = new int[N][cap + 1];
+        for (int i = 0; i <= cap; i++) {
+            memo[0][i] = i >= items[0][0] ? items[0][1] : 0;
         }
 
-        int res = bestValue(weight, values, index - 1, cap);
-        if (cap >= weight[index]) {
-            res = Math.max(res, values[index] + bestValue(weight, values, index - 1, cap - weight[index]));
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j <= cap; j++) {
+                // 0~i这些物品容积为j的背包获得的最大值
+                memo[i][j] = memo[i - 1][j];
+                if (j >= items[i][0]) {
+                    memo[i][j] = Math.max(memo[i][j], memo[i - 1][j - items[i][0]] + items[i][1]);
+                }
+            }
         }
 
-        memo[index][cap] = res;
-        return res;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < memo[i].length; j++) {
+                System.out.print("memo[" + i + ", " + j + "] = " + memo[i][j]);
+            }
+        }
+
+        return memo[N - 1][cap];
+    }
+
+    //优化一：两行轮流使用
+    int bestValueOpt1(int[][] items, int cap) {
+        if (cap <= 0 || items.length <= 0) {
+            return 0;
+        }
+        int N = items.length;
+        int[][] memo = new int[2][cap + 1];
+        for (int i = 0; i <= cap; i++) {
+            memo[0][i] = i >= items[0][0] ? items[0][1] : 0;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j <= cap; j++) {
+                memo[i % 2][j] = memo[(i - 1) % 2][j];
+                if (j >= items[i][0]) {
+                    memo[i % 2][j] = Math.max(memo[i % 2][j], memo[(i - 1) % 2][j - items[i][0]] + items[i][1]);
+                }
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < memo[i].length; j++) {
+                System.out.println("memo[" + i + ", " + j + "] = " + memo[i][j]);
+            }
+        }
+
+        return memo[(N - 1) % 2][cap];
+    }
+
+    //优化二；只使用一行，从右向左刷新内容
+    int bestValueOpt2(int[][] items, int cap) {
+        if (cap <= 0 || items.length <= 0) {
+            return 0;
+        }
+        int N = items.length;
+        int[] memo = new int[cap + 1];
+        for (int i = 0; i <= cap; i++) {
+            memo[i] = i >= items[0][0] ? items[0][1] : 0;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = cap; j >= items[i][0]; j--) {
+                memo[j] = Math.max(memo[j], memo[j - items[i][0]] + items[i][1]);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder("memo[");
+        for (int j = 0; j < memo.length; j++) {
+            sb.append(j == (memo.length - 1 ) ? memo[j] + "]" : memo[j] + ", ");
+        }
+        System.out.println(sb.toString());
+
+
+        return memo[cap];
     }
 }
